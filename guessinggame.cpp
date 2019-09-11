578e220f03c0cd172576141f89f12b8323a512cf
ryantmk518 <ryantmk518@gmail.com>
2019-09-11 08:39:22 -0700

    Comments added

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
-
+//Guessing Game. Random number is given, and user tries to guess the number.
 
 using namespace std;
 
 int main()
 {
-  srand(time(NULL));
-  bool start = true;
-  while (start == true) {
-    int input = 0;
+  srand(time(NULL)); 
+  bool start = true; //Game start
+  while (start == true) { //If player does not select do not play again
+    int input = 0; // Initialize variables
     start = false;
     int guesses = 1;
     bool play = true;
-    int random = (rand()%1000) +1;
-    char again = 'n';
-    while (play == true) {
-      cout << "Enter a noooomber between 1 and 1000" << endl;
-      cin >> input;
+    int random = (rand()%1000) +1; // Selects a random number between 1 and 1000
+    char again = 'n'; 
+    while (play == true) { //When the game is still going
+      cout << "Enter a noooomber between 1 and 1000" << endl; 
+      cin >> input; //Get input
       if (input == random) {
 	cout << "You guessed the right number in " << guesses << " tries!" << endl;
-	cout << "Play again? y/n" << endl;
-	cin >> again;
-	if (again == 'y') {
+	cout << "Play again? y/n" << endl; 
+	cin >> again; 
+	if (again == 'y') { //If player wants to play again
           play = false;
 	  start = true;
 	}
-	else {
+	else { //If player does not want to play again
 	  play = false;
 	  start = false;
 	}
       }
-      if (input > random) {
+      if (input > random) { //If the number is too big
 	cout << "Too high! Try again!" << endl;
 	play = true;
 	guesses++;
       }
-      if (input < random) {
+      if (input < random) { //If the number is too small
         cout << "Too low! Try again!" << endl;
 	play = true;
 	guesses++;
